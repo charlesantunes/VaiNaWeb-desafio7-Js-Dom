2244cfb7b1352624acc3f4abef6bdeaa5f63cc30
charlesantunes <charleswcantunes@gmail.com>
2022-07-07 20:13:30 -0300

    desenvolvendo o responsivel  mobile

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ const btn = document.querySelector(".btn");
 const tela = document.querySelector(".tela")
 
 function autenticar() {
-    if ((login.value) && (senha.value.length >=8)) {
+    if ((login.value) && (senha.value.length >=3)) {
         btn.removeAttribute("disabled");
         btn.classList.remove("btnColor2")
         btn.classList.add("btnColor1")
 
         //aqui está o mistério do botão, ja criei outra function onclick e nada.
+        
         tela.classList.remove("tela1");
         tela.classList.add("tela2");
 
     }else{
         btn.setAttribute("disabled", "disabled");
+        btn.classList.remove("btnColor1")
+        btn.classList.add("btnColor2")
     }
 }
 
